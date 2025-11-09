acbf8b781db0742eda8c8eb8b4d109752d005df2
Mick <mfrayne14@gmail.com>
2025-11-09 00:16:09 -0500

    default values given to settings, print_error function implemented, structure of process_args set up

--- highlighter.c
+++ highlighter.c
@@ -22,9 +22,9 @@ static const char* COLOR_RESET_COLOR = "\033[0m";
 
 //settings for highlighting determined by cli command options
 typedef struct Settings{
-        char* input_file;
-        char* output_file;
-        char* color;
+        FILE* input_file;
+        FILE* output_file;
+        const char* color;
         int no_color;
         char* prefix;
         char* postfix;
@@ -52,20 +52,68 @@ int main(int argc, char *argv[]) {
 	//write output to stdout or specified file
 	//write statistics to stdERR
 	
-	settings_t settings = {"","","",0,"","","",};
+	settings_t settings = {stdin,stdout,COLOR_RED,0,"","","",};
 
 	process_args(argc,argv,&settings);
+	
 	return 0;
 }
 
+void print_error(const char* error_message){
+	printf("Error: %s",error_message);
+}
 
+
+//helper function for process_args
+//used to valididate the the file path given is a valid file
+bool is_valid_file(char* path){
+	//check file path for valid file
+}
 int process_args(int argc,char* argv[], settings_t* settings){
 	for (int i = 1; i < argc; i++) {
-    		if (strncmp(argv[i], "-h\0",3) == 0 || strncmp(argv[i], "--help",7) == 0) {
-        		print_help();
-			return 0;
-    		}
+    		if (strncmp(argv[i], "-h\0",3) == 0 || strncmp(argv[i], "--help",6) == 0) {
+                        print_help();
+                        return 0;
+                }
+		else if(i == argc-1){
+			settings->text = argv[i];
+		}
+		//handles options 
+		else{
+			if (strncmp(argv[i], "-i\0",3) == 0 || strncmp(argv[i], "--input",7) == 0) {
+                       		i++;
+				// check following file is valid
+				if(is_valid_file(argv[i])){
+					//handle correct input
+				}
+				else{
+					//handle error of wrong file path and input error message 
+				}
+                	}
+			else if (strncmp(argv[i], "-o\0",3) == 0 || strncmp(argv[i], "--output",7) == 0) {
+                        	//check folling file is valid
+                        	return 0;
+                	}
+			else if (strncmp(argv[i], "-c\0",3) == 0 || strncmp(argv[i], "--color",7) == 0) {
+				//check following color is valid
+				//hanlde color 
+                	}
+			else if (strncmp(argv[i], "--no-color",7) == 0) {
+				//handle no color
+    			}
+			else if (strncmp(argv[i], "--prefix",7) == 0) {
+				//handle prefix
+			}
+			else if (strncmp(argv[i], "--help",7) == 0) {
+				//handle postfix
+			}
+			else{
+				print_error("invalid argument");
+				exit(-1);
+			}
+		}
 	}
+	return 0;
 }
 
 
@@ -95,6 +143,7 @@ void print_help(){
 	printf("  ./highlighter\n");
 	printf("  ./highlighter --prefix \">>\" --postfix \"<<\" \"keyword\" -o out.txt (highlights \"keyword\" with"); 
 		printf(" prefix \">>\" and postfix \"<<\" in red from STDIN to out.txt)\n");
+	exit(0);
 }
 
 
